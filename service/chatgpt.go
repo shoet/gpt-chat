3d52dd108fb59148c1634df1fad4d35aca45e81f
shoet <32211937+shoet@users.noreply.github.com>
2023-10-07 08:51:22 +0900

    リファクタリング

--- service/chatgpt.go
+++ service/chatgpt.go
@@ -27,23 +27,27 @@ func NewChatGPTService(apiKey string, client interfaces.Client) *ChatGPTService 
 }
 
 func (c *ChatGPTService) Chat(input string) ([]byte, error) {
-	messages := []models.ChatGPTRequestMessage{
-		{
-			Role:    "user",
-			Content: input,
-		},
-		{
-			Role:    "system",
-			Content: "ユーザーからの要求分に最も適した回答を提供して下さい。",
-		},
+	req, err := c.buildRequestWithStream(input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build request: %w", err)
 	}
+	resp, err := c.executeRequestWithStream(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute request: %w", err)
+	}
+	return resp, nil
+}
 
+func (c *ChatGPTService) buildRequestWithStream(input string) (*http.Request, error) {
+	messages := []models.ChatGPTRequestMessage{
+		{Role: "user", Content: input},
+		{Role: "system", Content: "ユーザーからの要求分に最も適した回答を提供して下さい。"},
+	}
 	requestBody := models.ChatGPTRequest{
 		Model:    "gpt-3.5-turbo",
 		Messages: messages,
 		Stream:   true,
 	}
-
 	b, err := json.Marshal(requestBody)
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal request body: %w", err)
@@ -57,21 +61,15 @@ func (c *ChatGPTService) Chat(input string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Cache-Control", "no-cache")
-	req.Header.Set("Accept", "text/event-stream")
-	req.Header.Set("Connection", "keep-alive")
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.apiKey))
+	return req, nil
+}
 
-	res, err := c.client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %w", err)
-	}
-	defer res.Body.Close()
-
+func (c *ChatGPTService) executeRequestWithStream(req *http.Request) ([]byte, error) {
 	header := "data:"
 	var buffer []byte
-	handler := func(b []byte) error {
+	chunkedCallback := func(b []byte) error {
 		w := os.Stdout
 		if strings.HasPrefix(string(b), header) {
 			b = []byte(strings.TrimPrefix(string(b), header))
@@ -94,11 +92,57 @@ func (c *ChatGPTService) Chat(input string) ([]byte, error) {
 		return nil
 	}
 
-	if err := SSERead(res.Body, handler); err != nil {
+	sseClient := &SSEClient{client: c.client}
+	if err := sseClient.Request(req, "\n\n", chunkedCallback); err != nil {
 		return nil, fmt.Errorf("failed to read SSE: %w", err)
 	}
 
 	return buffer, nil
+}
+
+type SSEClient struct {
+	client interfaces.Client
+}
+
+func (c *SSEClient) Request(req *http.Request, chunkSep string, chunkHandler func(b []byte) error) error {
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Accept", "text/event-stream")
+	req.Header.Set("Connection", "keep-alive")
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	defer res.Body.Close()
+
+	chEve := make(chan string)
+	chErr := make(chan error)
+	scanner := SplitScanner(res.Body, chunkSep)
+	go func() {
+		for scanner.Scan() {
+			b := scanner.Bytes()
+			chEve <- string(b)
+		}
+		if err := scanner.Err(); err != nil {
+			chErr <- err
+			return
+		}
+		chErr <- io.EOF
+	}()
+
+	for {
+		select {
+		case err := <-chErr:
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		case event := <-chEve:
+			if err := chunkHandler([]byte(event)); err != nil {
+				return err
+			}
+		}
+	}
 }
 
 func SplitScanner(r io.Reader, sep string) *bufio.Scanner {
@@ -123,35 +167,4 @@ func SplitScanner(r io.Reader, sep string) *bufio.Scanner {
 	}
 	scanner.Split(split)
 	return scanner
-}
-
-func SSERead(reader io.Reader, handler func(b []byte) error) error {
-	chEve := make(chan string)
-	chErr := make(chan error)
-	scanner := SplitScanner(reader, "\n\n")
-	go func() {
-		for scanner.Scan() {
-			b := scanner.Bytes()
-			chEve <- string(b)
-		}
-		if err := scanner.Err(); err != nil {
-			chErr <- err
-			return
-		}
-		chErr <- io.EOF
-	}()
-
-	for {
-		select {
-		case err := <-chErr:
-			if err == io.EOF {
-				return nil
-			}
-			return err
-		case event := <-chEve:
-			if err := handler([]byte(event)); err != nil {
-				return err
-			}
-		}
-	}
 }
